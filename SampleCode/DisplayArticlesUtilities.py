ab7c2cdbe72bc89f189615a47ad0e6a5981eaac5
acraigsen <acraigsen@gmail.com>
2024-04-11 19:36:30 -0400

    Reorganized display format

--- SampleCode/DisplayArticlesUtilities.py
+++ SampleCode/DisplayArticlesUtilities.py
@@ -6,17 +6,14 @@ def DisplayArticleInfo(articles):
     #Display Article Info as a formatted list with numbers and content by looping through the list and displaying each dictionary item
     for i in range(len(articles)):
         print(f"{i+1}. Title: {articles[i]['title']}")
+        print(f"    Author: {articles[i]['author']}")
         print(f"    Description: {articles[i]['description']}")
-        print(f"    URL: {articles[i]['content']}")
+        print(f"    Content: {articles[i]['content']}")
         print(f"    URL: {articles[i]['url']}")
         print(f"    Source: {articles[i]['source']}")
         print(f"    Published Date: {articles[i]['published_date']}")
-        print(f"    Author: {articles[i]['author']}")
         print(f"    Image: {articles[i]['image']}")
         print(f"    Sentiment: {articles[i]['sentiment']}")
-        print(f"    URL: {articles[i]['keywords']}")
+        print(f"    Keywords: {articles[i]['keywords']}")
         print("\n")
     
-#'content', 'sentiments', 'keywords', 'descriptions', 'urls', 'sources', 'published_dates', 'authors', and 'images'
-
-# [{'title': titles[i], 'description': descriptions[i], 'url': urls[i], 'content': content[i], 'source': sources[i], 'published_date': published_dates[i], 'author': authors[i], 'image': images[i], 'sentiment': sentiments[i]} for i in range(len(titles))]
