7fbfa4a6d6d1799f46db2f03e9c86c9b5b211706
acraigsen <acraigsen@gmail.com>
2024-04-15 20:59:15 -0400

    Add search term to article lookup

--- SampleCode/ArticleImportUtils.py
+++ SampleCode/ArticleImportUtils.py
@@ -8,24 +8,53 @@ from dotenv import load_dotenv
 from newsapi import NewsApiClient
 
 #Create the function to pull the article data passing in country and language
-def get_top_headlines(country = None, language = None):
+def get_top_headlines(country = None, language = None, query = None):
     
     #Load the API key from the .env file
     load_dotenv()
-    news_api_key = os.environ.get('NewsAPIKey')
+    news_api_key = os.environ.get('NewsAPIKey2')
     
     #Create the NewsApiClient object
     newsapi = NewsApiClient(news_api_key)
     
     #Get the top headlines from the country and language
     if country == None and language == None:
-        top_headlines = newsapi.get_top_headlines()
+        top_headlines = newsapi.get_top_headlines(q=query)
     elif country == None:
-        top_headlines = newsapi.get_top_headlines(language=language)
+        top_headlines = newsapi.get_top_headlines(q=query,language=language)
     elif language == None:
-        top_headlines = newsapi.get_top_headlines(country=country)
+        top_headlines = newsapi.get_top_headlines(q=query,country=country)
     else:
-        top_headlines = newsapi.get_top_headlines(country=country, language=language)
+        top_headlines = newsapi.get_top_headlines(q=query,country=country, language=language)
     
     #Return the top headlines
     return top_headlines
+
+
+
+
+#Define a function to create a list of the number of articles for each country and language combination
+def getNumberOfArticles(validCountryCodes, validLanguageCodes):
+    #Create a dictionary to hold the number of articles for each country and language combination
+
+    numArticles = {}
+    
+    #Get the number of articles for each country and language combination
+    for country in validCountryCodes:
+        if country!= "None":
+            print("Getting number of articles for country: " + country)
+            for language in validLanguageCodes:
+                if language != "None":
+                    print("Getting number of articles for language: " + language)
+                    top_headlines = get_top_headlines(country, language)
+                    numArticles[(country, language)] = len(top_headlines['articles'])
+                else:
+                    print("Skipping 'None' language code")
+        else:
+            print("Skipping 'None' country code")
+    
+        
+    
+    #Return the dictionary of the number of articles for each country and language combination
+    return numArticles
+    
