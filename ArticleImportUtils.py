2848254b457e77d6797dd47d5e57705d54f95425
acraigsen <acraigsen@gmail.com>
2024-04-19 15:27:08 -0400

    Updated API and response

--- ArticleImportUtils.py
+++ ArticleImportUtils.py
@@ -8,12 +8,55 @@ from langdetect import detect
 from dotenv import load_dotenv
 from newsapi import NewsApiClient
 
+def get_article_data(country = None, language = None, query = None):
+    load_dotenv()
+
+    if query == None or query == "None" or query == "":
+        query = "News"
+    
+    print("Getting article data for query: " + query)
+
+    wrld_news_api_key = os.environ.get('WrldNewsAPIKey')
+
+    url = "https://api.worldnewsapi.com/search-news"
+
+    params = {}
+
+    # Add parameters only if they are provided and not None
+    if query:
+        params['text'] = query
+    if country and country != '':
+        params['source-countries'] = country
+    if language and language != '':
+        params['language'] = language
+
+    print(params)
+
+    headers = {
+        'x-api-key': wrld_news_api_key
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return f"Error: {response.status_code}"
+
+if __name__ == "__main__":
+    print(get_article_data())
+
+
+
+
+
 #Create the function to pull the article data passing in country and language
 def get_top_headlines(country = None, language = None, query = None):
     
     #Load the API key from the .env file
     load_dotenv()
     news_api_key = os.environ.get('NewsAPIKey2')
+    wrld_news_api_key = os.environ.get('WrldNewsAPIKey')
     
     #Create the NewsApiClient object
     newsapi = NewsApiClient(news_api_key)
--- DisplayArticlesUtilities.py
+++ DisplayArticlesUtilities.py
@@ -15,10 +15,10 @@ def DisplayArticleInfo(articles):
         for i in range(len(articles)):
             print(f"{i+1}. Title: {articles[i]['title']}")
             print(f"    Author: {articles[i]['author']}")
-            print(f"    Description: {articles[i]['description']}")
+            #print(f"    Description: {articles[i]['description']}")
             print(f"    Content: {articles[i]['content']}")
             print(f"    URL: {articles[i]['url']}")
-            print(f"    Source: {articles[i]['source']}")
+            #print(f"    Source: {articles[i]['source']}")
             print(f"    Published Date: {articles[i]['published_date']}")
             print(f"    Image: {articles[i]['image']}")
             print(f"    Sentiment: {articles[i]['sentiment']}")
@@ -31,32 +31,32 @@ def DisplayArticleInfo(articles):
 #Define a function to crete a list of dictionaries of articles
 def CreateArticleList(top_headlines):
     #Get News Articles
-    articles = top_headlines['articles']
+    articles = top_headlines['news']
 
     
     #Get Article Titles
     titles = [article['title'] for article in articles]
 
     #Get Article Descriptions
-    descriptions = [article['description'] for article in articles]
+    #descriptions = [article['description'] for article in articles]
 
     #Get Article URLs
     urls = [article['url'] for article in articles]
 
     #Get Article Content
-    content = [article['content'] for article in articles]
+    content = [article['text'] for article in articles]
 
     #Get Article Sources
-    sources = [article['source']['name'] for article in articles]
+    #sources = [article['source']['name'] for article in articles]
 
     #Get Article Published Dates
-    published_dates = [article['publishedAt'] for article in articles]
+    published_dates = [article['publish_date'] for article in articles]
 
     #Get Article Authors
-    authors = [article['author'] for article in articles]
+    authors = [article['authors'] for article in articles]
 
     #Get Article Images
-    images = [article['urlToImage'] for article in articles]
+    images = [article['image'] for article in articles]
 
     #Get Article Sentiments
     nlp = pipeline('sentiment-analysis')
@@ -65,17 +65,17 @@ def CreateArticleList(top_headlines):
 
     #Get Article Keywords
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-    keywords = [tokenizer.tokenize(article['title']) for article in articles]
+    keywords = [tokenizer.tokenize(article['text']) for article in articles]
 
     #Combine Article Info into a dictionary
     for i in range(len(articles)):
         articles[i] = {
             'title': titles[i],
             'author': authors[i],
-            'description': descriptions[i],
+            #'description': descriptions[i],
             'content': content[i],
             'url': urls[i],
-            'source': sources[i],
+            #'source': sources[i],
             'published_date': published_dates[i],
             'sentiment': sentiments[i],
             'keywords': keywords[i],
@@ -90,10 +90,10 @@ def DisplayTranslatedArticle(translated_article):
     print("Translated Article:" + "\n\n")
     print(f"Translated Title: {translated_article[0]['title']}\n")
     print(f"  Author: {translated_article[0]['author']}\n")
-    print(f"  Description: {translated_article[0]['description']}\n")
+    #print(f"  Description: {translated_article[0]['description']}\n")
     print(f"  Content: {translated_article[0]['content']}\n")
     print(f"  URL: {translated_article[0]['url']}\n")
-    print(f"  Source: {translated_article[0]['source']}\n")
+    #print(f"  Source: {translated_article[0]['source']}\n")
     print(f"  Published Date: {translated_article[0]['published_date']}\n")
     print(f"  Image: {translated_article[0]['image']}\n")
     print(f"  Sentiment: {translated_article[0]['sentiment']}\n")
